ecba0fa21a7156c407f44baba84e46d583db89a3
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-04-14 17:22:09 +0800

    Update guild.js

--- commands/guild.js
+++ commands/guild.js
@@ -4,22 +4,22 @@ module.exports.run = async (bot, message, args, request) => {
   
         const guildName = args.join(" ");
         const urlMain = "https://api.wynncraft.com/public_api.php?action=guildStats&command=" + (guildName);
-        request(urlMain, function(err, response, guild) {
-            if(err) {
-                console.log(err);
-                return message.channel.send('在查詢時出了點問題:P');
-            }
-            guild = JSON.parse(guild);
-            if(guild.error){
-                message.channel.send("你輸入了錯誤的工會名稱,可能是大小寫錯誤了")
-            }else{
-                let guildInfo = new Discord.RichEmbed()
-                    .setColor("RANDOM")
-                    .setTitle(`${guild.name} 的資訊`)
-                message.channel.send(guildInfo)
-            }
-        })
-    }    
+            request(urlMain,async function(err, response, guild) {
+                if(err) {
+                    console.log(err);
+                    return message.channel.send('在查詢時出了點問題:P');
+                }
+                guild = JSON.parse(guild);
+                if(guild.error){
+                    message.channel.send("你輸入了錯誤的工會名稱,可能是大小寫錯誤了")
+                }else{
+                    let guildInfo = new Discord.RichEmbed()
+                        .setColor("RANDOM")
+                        .setTitle(`${guild.name} 的資訊`)
+                    message.channel.send(guildInfo)
+                }
+            })
+        }  
 //moment.duration(guild.duration).format(" D [天], H [時], m [分], s [秒]")
 
 module.exports.help = {
