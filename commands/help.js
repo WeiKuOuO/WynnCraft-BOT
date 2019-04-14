71ceaa491a6b50a3140776a0b902188772d242e9
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-04-14 17:44:39 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
         .setFooter(bot.user.username, bot.user.avatarURL)
         .setTitle("指令列表")
         .addField('• 指令', "```py\n1# wc/help\n2# wc/guild```",true )
-        .addField('• 說明', "```fix\n開啟此列表```\n查詢公會資訊```",true)
+        .addField('• 說明', "```fix\n開啟此列表\n查詢公會資訊```",true)
         .addField('• 建議', "(Admin) 代表需要Admin權限\n如果有任何有關於指令的建議，像是想要新增指令或是功能\n還歡迎聯絡 ***WeiKu#3402***\n我會很樂意為你服務的\n[WynnCraft中文討論群](https://wynncrafttw.tk/dc) | [WynnCraft中文資訊站官網](https://wynncrafttw.tk) | [機器人邀請碼](https://wynncrafttw.tk/bot)\n如果可以的話請把Bot邀請到你的Discord群~~" )
         .setTimestamp(new Date())
         .setFooter(`${message.author.tag}`);
