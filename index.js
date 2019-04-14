26011443a58ef74f55af7aaee0ec03aa8058afc9
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-04-14 17:34:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ bot.on('ready', function() {
         `機器人製作 | 微苦 ♪`,
         `歡迎使用WynnCraft中文資訊站 ♪`
       ]
-      bot.user.setActivity(statuslist[index], { type: "STREAMING", url: "https://www.youtube.com/c/微苦/live"});
+      bot.user.setActivity(statuslist[index], { type: "STREAMING", url: "https://www.twitch.tv/weikuouo"});
       index++
       if (index === statuslist.length) index = 0;
   }, 3000)
