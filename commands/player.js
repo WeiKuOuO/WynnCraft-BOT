ca83a4702a6c47b2cc3adcb45245abeb61960bfe
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-04-23 23:35:14 +0800

    Update player.js

--- commands/player.js
+++ commands/player.js
@@ -12,12 +12,13 @@ module.exports.run = async (bot, message, args) => {
                 } 
                 player = JSON.parse(player);
                 console.log(player.data)
-                if(!player.data[0]['username']){
+                console.log(player.message)
+                if(!player.data){
                     message.channel.send("你輸入了錯誤的玩家名稱,請重新確認是否有打錯ID")
                 }else{
                     let playerInfo = new Discord.RichEmbed()
                         .setColor(0x34AB00)
-                        .setTitle(`${player.data[0]['username']} 的資訊`)
+                        .setTitle(`${player} 的資訊`)
                     message.channel.send(playerInfo)
                 }
             })
