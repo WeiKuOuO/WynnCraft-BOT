d4abf0f36af795aa9c9b287dfd9fea996bfeb26e
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-04-23 23:47:55 +0800

    Update player.js

--- commands/player.js
+++ commands/player.js
@@ -4,28 +4,18 @@ var request = require('request');
 module.exports.run = async (bot, message, args) => {
   
         const playerName = args.join(" ");
-        const urlMain = `https://api.wynncraft.com/v2/player/${playerName}/stats`;
+        const urlMain = `https://api.wynncraft.com/v2/player/${playerName}/stats`
             request(urlMain, function(err, response, player) {
                 if(err) {
                     console.log(err);
                     return message.channel.send('在查詢時出了點問題:P');
-                } 
-                playerJSON = JSON.parse(player);
-                console.log(playerJSON.data)
-                console.log(playerJSON.message)
-                console.log(playerJSON.kind)
-                console.log(playerJSON)
-                if(!player.data){
-                    message.channel.send("你輸入了錯誤的玩家名稱,請重新確認是否有打錯ID")
-                }else{
-                    let playerInfo = new Discord.RichEmbed()
-                        .setColor(0x34AB00)
-                        .setTitle(`${player} 的資訊`)
-                    message.channel.send(playerInfo)
                 }
+                player = JSON.parse(player);
+                console.log(player.data)
             })
         }  
+//moment.duration(guild.duration).format(" D [天], H [時], m [分], s [秒]")
 
 module.exports.help = {
-  name: 'player',
+  name: 'guild',
 };
