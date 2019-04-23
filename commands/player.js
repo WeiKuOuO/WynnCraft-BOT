2370df8a8d621db95cc429d2680776325876f4ab
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-04-23 22:55:27 +0800

    Create player.js

---
+++ commands/player.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js")
+var request = require('request');
+
+module.exports.run = async (bot, message, args) => {
+  
+        const playerName = args.join(" ");
+        const urlMain = `https://api.wynncraft.com/v2/player/${playerName}/stats`;
+            request(urlMain, function(err, response, player) {
+                if(err) {
+                    console.log(err);
+                    return message.channel.send('在查詢時出了點問題:P');
+                }
+                player = JSON.parse(player);
+                if(!player.data.username){
+                    message.channel.send(player.data.uuid)
+                    message.channel.send("你輸入了錯誤的玩家名稱,請重新確認是否有打錯ID")
+                }else{
+                    let playerInfo = new Discord.RichEmbed()
+                        .setColor(0x34AB00)
+                        .setTitle(`${player.data.username} 的資訊`)
+                    message.channel.send(playerInfo)
+                }
+            })
+        }  
+
+module.exports.help = {
+  name: 'player',
+};
