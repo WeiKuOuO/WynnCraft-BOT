5e4126b6c34802278d6c00c23187b157088e350a
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-04-24 23:50:30 +0800

    Update player.js

--- commands/player.js
+++ commands/player.js
@@ -11,10 +11,8 @@ module.exports.run = async (bot, message, args) => {
                     return message.channel.send('在查詢時出了點問題:P');
                 }
                 player = JSON.parse(player);
-                console.log(player.data[0['username']])
+                console.log(player.data[0[username]])
                 console.log(urlMain)
-                console.log(player.data[0].username)
-                console.log(player.data.username)
             })
         }  
 //moment.duration(guild.duration).format(" D [天], H [時], m [分], s [秒]")
