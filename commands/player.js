f3e82dd2380750c10ef33002ce602dd00f5991ff
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-04-25 22:32:06 +0800

    Update player.js

--- commands/player.js
+++ commands/player.js
@@ -3,20 +3,22 @@ var request = require('request');
 
 module.exports.run = async (bot, message, args) => {
   
-        const playerName = args.join(" ");
+        const playerName = args.join("");
         const urlMain = `https://api.wynncraft.com/v2/player/${playerName}/stats`
+        if(!playerName == "")
             request(urlMain, function(err, response, player) {
                 if(err) {
                     console.log(err);
                     return message.channel.send('在查詢時出了點問題:P');
                 }
                 player = JSON.parse(player);
-                console.log(player.data[0].username)
                 console.log(urlMain)
+                console.log(player.data[0].username)
+                
             })
         }  
 //moment.duration(guild.duration).format(" D [天], H [時], m [分], s [秒]")
-
+ 
 module.exports.help = {
   name: 'guild',
 };
