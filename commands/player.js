77361d3112f932a5d8636e6ff645bb5bd5b1445f
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-04-25 22:45:47 +0800

    Update player.js

--- commands/player.js
+++ commands/player.js
@@ -5,19 +5,32 @@ module.exports.run = async (bot, message, args) => {
   
         const playerName = args.join("");
         const urlMain = `https://api.wynncraft.com/v2/player/${playerName}/stats`
-        if(!playerName == "")
+        if(!playerName == ""){
+            message.channel.send("你並未輸入玩家名稱...")
+            return
+        }
             request(urlMain, function(err, response, player) {
                 if(err) {
                     console.log(err);
                     return message.channel.send('在查詢時出了點問題:P');
                 }
                 player = JSON.parse(player);
-                console.log(urlMain)
-                console.log(player.data[0].username)
-                
+                if(!player.data[0].username){
+                    message.channel.send("你輸入了錯誤的玩家ID")
+                }else{
+                    let guildInfo = new Discord.RichEmbed()
+                        .setColor(0x34AB00)
+                        .setTitle(`${player.data[0].username} 的資訊`)
+                        // .addField(":pager:  工會名稱",`\`\`\`css\n${guild.name}\`\`\``,true)
+                        // .addField(":mega: 公會前綴",`\`\`\`fix\n${guild.prefix}\`\`\``,true)
+                        // .addField(":evergreen_tree: 公會等級",`\`\`\`diff\n+    Level${guild.level}   +\`\`\``,true)
+                        // .addField(":earth_asia: 領地數量",`\`\`\`fix\n${guild.territories}\`\`\``,true)
+                        // .addField(":calendar_spiral: 創建日期",`\`\`\`xl\n${guild.createdFriendly}\`\`\``,true)
+                        // .addField(":seedling: 經驗值",`\`\`\`diff\n-    ${guild.xp}%    -\`\`\``,true)
+                    message.channel.send(guildInfo)
+                }
             })
         }  
-//moment.duration(guild.duration).format(" D [天], H [時], m [分], s [秒]")
  
 module.exports.help = {
   name: 'guild',
