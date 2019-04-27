f6a960a66ef894dbb5a29b442d3092e3df64bfa8
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-04-27 17:23:59 +0800

    Update player.js

--- commands/player.js
+++ commands/player.js
@@ -13,54 +13,58 @@ module.exports.run = async (bot, message, args) => {
                     return message.channel.send('在查詢時出了點問題:P');
                 }
                 player = JSON.parse(player);
-                const xp0 = [
-                  player.data[0].classes[0].professions.combat,
-                  player.data[0].classes[0].professions.woodcutting,
-                  player.data[0].classes[0].professions.mining,
-                  player.data[0].classes[0].professions.fishing,
-                  player.data[0].classes[0].professions.farming,
-                  player.data[0].classes[0].professions.alchemism,
-                  player.data[0].classes[0].professions.armouring,
-                  player.data[0].classes[0].professions.cooking,
-                  player.data[0].classes[0].professions.jeweling,
-                  player.data[0].classes[0].professions.scribing,
-                  player.data[0].classes[0].professions.tailoring,
-                  player.data[0].classes[0].professions.weaponsmithing,
-                  player.data[0].classes[0].professions.woodworking,
-                  player.data[0].classes[0].professions.profession,
-                  player.data[0].classes[0].professions.overall
-                ]
-                const xp = [
-                  xp0,
-                  xp1,
-                  xp2
-                ]
-                const text = [
-                  `\`\`\`css\n戰鬥 / Combat | [${xp[0][0].level} - ${xp[0][0].xp}]\`\`\``
-                ]  
-                if(!player.data[0].classes[0].name){
-                   message.channel.send("此玩家並沒有創建角色")
-                } else {
-                  if(player.data[0].classes[4].name){
-                    
-                  }else{
-                    if(player.data[0].classes[3].name){
-                    
+                if(player.message == "Bad Request"){
+                  message.channel.send("你輸入了錯誤的玩家ID")
+                }else{
+                  const xp0 = [
+                    player.data[0].classes[0].professions.combat,
+                    player.data[0].classes[0].professions.woodcutting,
+                    player.data[0].classes[0].professions.mining,
+                    player.data[0].classes[0].professions.fishing,
+                    player.data[0].classes[0].professions.farming,
+                    player.data[0].classes[0].professions.alchemism,
+                    player.data[0].classes[0].professions.armouring,
+                    player.data[0].classes[0].professions.cooking,
+                    player.data[0].classes[0].professions.jeweling,
+                    player.data[0].classes[0].professions.scribing,
+                    player.data[0].classes[0].professions.tailoring,
+                    player.data[0].classes[0].professions.weaponsmithing,
+                    player.data[0].classes[0].professions.woodworking,
+                    player.data[0].classes[0].professions.profession,
+                    player.data[0].classes[0].professions.overall
+                  ]
+                  const xp = [
+                    xp0,
+                    xp1,
+                    xp2
+                  ]
+                  const text = [
+                    `\`\`\`css\n戰鬥 / Combat | [${xp[0][0].level} - ${xp[0][0].xp}]\`\`\``
+                  ]  
+                  if(!player.data[0].classes[0].name){
+                     message.channel.send("此玩家並沒有創建角色")
+                  } else {
+                    if(player.data[0].classes[4].name){
+                      
                     }else{
-                      if(player.data[0].classes[2].name){
-                    
+                      if(player.data[0].classes[3].name){
+                      
                       }else{
-                        if(player.data[0].classes[1].name){
-                          var role1
-                          if(player.data[0].classes[1].name == "assassin"){
-                            var role1 = ":dagger: 刺客 / Assassin"
-                          }
-                          if(player.data[0].classes[1].name == "assassin"){
-                            var role1 = ":dagger: 刺客 / Assassin"
-                            let playerInfo = new Discord.RichEmbed()
-                              .setTitle(`${player.data[0].username} 的玩家資訊`)
-                              .addField(`${role1} 的等級資訊`,text,true)
-                            message.channel.send(playerInfo)
+                        if(player.data[0].classes[2].name){
+                      
+                        }else{
+                          if(player.data[0].classes[1].name){
+                            var role1
+                            if(player.data[0].classes[1].name == "assassin"){
+                              var role1 = ":dagger: 刺客 / Assassin"
+                            }
+                            if(player.data[0].classes[1].name == "assassin"){
+                              var role1 = ":dagger: 刺客 / Assassin"
+                              let playerInfo = new Discord.RichEmbed()
+                                .setTitle(`${player.data[0].username} 的玩家資訊`)
+                                .addField(`${role1} 的等級資訊`,text,true)
+                              message.channel.send(playerInfo)
+                            }
                           }
                         }
                       }
