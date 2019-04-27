3a0c3aa09cedb7d18a24d47f8b7c20a8d29c6b0c
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-04-27 11:13:33 +0800

    Update player1.js

--- commands/player1.js
+++ commands/player1.js
@@ -15,43 +15,7 @@ module.exports.run = async (bot, message, args) => {
                 const classes = args[1]
                 player = JSON.parse(player);
 
-                const profession = [
-                  player.data[0].classes[0].professions
-                ]
-                if(player.data[0].classes[1].professions){
-                  var profession = [
-                    player.data[0].classes[0].professions,
-                    player.data[0].classes[1].professions,
-                  ]
-                }
-                if(player.data[0].classes[2].professions){
-                  var profession = [
-                    player.data[0].classes[0].professions,
-                    player.data[0].classes[1].professions,
-                    player.data[0].classes[2].professions
-                  ]
-                }
-                const xp0 = [
-                  profession[0].combat,
-                  profession[0].woodcutting,
-                  profession[0].mining,
-                  profession[0].fishing,
-                  profession[0].farming,
-                  profession[0].alchemism,
-                  profession[0].armouring,
-                  profession[0].cooking,
-                  profession[0].jeweling,
-                  profession[0].scribing,
-                  profession[0].tailoring,
-                  profession[0].weaponsmithing,
-                  profession[0].woodworking,
-                  profession[0].profession,
-                  profession[0].overall
-                ]
-                const text = [
-                  `\`\`\`css\n戰鬥 / Combat | [${xp0[1].level} - ${xp0[1].xp}]`
-                ]
-
+              
                 let onlinecheck = player.data[0].meta.location.online
                 if ( typeof onlinecheck !== 'false' && onlinecheck )
                 {
