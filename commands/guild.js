f2228966c92aab5eaa1400343387ab4aa5a04bdf
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-04-27 17:53:07 +0800

    Update guild.js

--- commands/guild.js
+++ commands/guild.js
@@ -60,6 +60,53 @@ module.exports.run = async (bot, message, args) => {
                             }
                         }
                     });
+                    let pages = [
+                        'test1 fuck u', 
+                        'test2 fuck u 2', 
+                        'test3 did u think i will say fuck u 3?', 
+                        'test4 Maybe', 
+                        '**test5 I want to say fuck u 10**'
+                       ]; 
+                       let page = 1; 
+                      
+                       const embed = new Discord.RichEmbed() 
+                         .setColor(0xffffff)
+                         .setFooter(`Page ${page} of ${pages.length}`) 
+                         .setDescription(pages[page-1])
+                      
+                       message.channel.send(embed).then(msg => { 
+                        
+                         msg.react('⏪').then( r => { 
+                           msg.react('⏩') 
+                          
+                           const backwardsFilter = (reaction, user) => reaction.emoji.name === '⏪' && user.id === message.author.id;
+                           const forwardsFilter = (reaction, user) => reaction.emoji.name === '⏩' && user.id === message.author.id; 
+                          
+                           const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 }); 
+                           const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 }); 
+                          
+                           
+                           backwards.on('collect', r => { 
+                             if (page === 1) return; 
+                             page--; 
+                             embed.setDescription(pages[page-1]); 
+                             embed.setFooter(`Page ${page} of ${pages.length}`); 
+                             msg.delete()
+                             msg.channel.send(embed) 
+                           })
+                          
+                           forwards.on('collect', r => { 
+                             if (page === pages.length) return; 
+                             page++; 
+                             embed.setDescription(pages[page-1]); 
+                             embed.setFooter(`Page ${page} of ${pages.length}`); 
+                             msg.delete()
+                             msg.channel.send(embed) 
+                           })
+                        
+                         })
+                      
+                       })
                     let guildInfo = new Discord.RichEmbed()
                         .setColor(0x34AB00)
                         .setTitle(`${guild.name} 的資訊`)
