3a33ea4fbfab3f3572bf11536faddaab14be5ecf
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-04-27 17:21:22 +0800

    Update player.js

--- commands/player.js
+++ commands/player.js
@@ -13,11 +13,6 @@ module.exports.run = async (bot, message, args) => {
                     return message.channel.send('在查詢時出了點問題:P');
                 }
                 player = JSON.parse(player);
-                const xp = [
-                  xp0,
-                  xp1,
-                  xp2
-                ]
                 const xp0 = [
                   player.data[0].classes[0].professions.combat,
                   player.data[0].classes[0].professions.woodcutting,
@@ -34,6 +29,11 @@ module.exports.run = async (bot, message, args) => {
                   player.data[0].classes[0].professions.woodworking,
                   player.data[0].classes[0].professions.profession,
                   player.data[0].classes[0].professions.overall
+                ]
+                const xp = [
+                  xp0,
+                  xp1,
+                  xp2
                 ]
                 const text = [
                   `\`\`\`css\n戰鬥 / Combat | [${xp[0][0].level} - ${xp[0][0].xp}]\`\`\``
