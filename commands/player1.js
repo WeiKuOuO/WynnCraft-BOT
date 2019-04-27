e0b8552cc8791255d1f1db6fecb44792a40f4b94
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-04-27 11:13:07 +0800

    .

--- commands/player1.js
+++ commands/player1.js
@@ -12,6 +12,7 @@ module.exports.run = async (bot, message, args) => {
                     console.log(err);
                     return message.channel.send('在查詢時出了點問題:P');
                 }
+                const classes = args[1]
                 player = JSON.parse(player);
 
                 const profession = [
@@ -49,7 +50,29 @@ module.exports.run = async (bot, message, args) => {
                 ]
                 const text = [
                   `\`\`\`css\n戰鬥 / Combat | [${xp0[1].level} - ${xp0[1].xp}]`
-                ]  
+                ]
+
+                let onlinecheck = player.data[0].meta.location.online
+                if ( typeof onlinecheck !== 'false' && onlinecheck )
+                {
+                  var onlineserver = player.data[0].meta.location.server
+                }
+                else
+                {
+                  var onlineserver = `玩家名: ${player.data[0].username} 不在線上`
+                }
+                if(player.message == "Bad Request"){
+                    message.channel.send("你輸入了錯誤的玩家ID")
+                }else{
+                  let guildInfo = new Discord.RichEmbed()
+                      .setColor(0x34AB00)
+                      .setTitle(`${player.data[0].username} 的資訊`)
+                      .addField(`${player.data[0].classes[0].name}`,text[1],true)
+                      .addField("等級",`\`\`\`css\n${player.data[0].classes[0].level}\`\`\``)
+                      .addField("所在分流",`\`\`\`css\n${onlineserver}\`\`\``) 
+                  message.channel.send(guildInfo)
+
+                }
             })
         }
     }
