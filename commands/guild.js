9697a575fe96f7924c506cea4c071ef9f3e0ded4
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-04-27 22:00:22 +0800

    Update guild.js

--- commands/guild.js
+++ commands/guild.js
@@ -83,32 +83,55 @@ module.exports.run = async (bot, message, args) => {
                         STOP: '⏹',
                     }
                     
-                    const collector = new Discord.ReactionCollector(message, (reaction, user) => Object.values(reactionControls).includes(reaction.emoji.name), {
-                        time: 60000, // stop automatically after one minute 
-                    });
-                    
-                    collector.on('collect', (reaction, user) => {
-                        switch (reaction.emoji.name) {
-                            case emojis.NEXT_PAGE: {
-                                // code for displaying next page here
-                                message.channel.send("hello")
-                                break; 
-                            }
-                            case emojis.PREV_PAGE: {
-                                // code for displaying previous page here
-                                message.channel.send("hello2")
-                                break;
-                            }
-                            case emojis.STOP: {
-                                // stop listening for reactions
-                                collector.stop();
-                                break;
-                            }
-                                
-                    }});
-                    collector.on('stop', async () => {
-                        await message.clearReactions();
-                    });
+                    let pages = [
+                        'test1 fuck u', 
+                        'test2 fuck u 2', 
+                        'test3 did u think i will say fuck u 3?', 
+                        'test4 Maybe', 
+                        '**test5 I want to say fuck u 10**'
+                    ]; 
+                    let page = 1; 
+                      
+                    const embed = new Discord.RichEmbed() 
+                        .setColor(0xffffff)
+                        .setFooter(`Page ${page} of ${pages.length}`) 
+                        .setDescription(pages[page-1])
+                      
+                     message.channel.send(embed).then(msg => { 
+                        
+                        msg.react('◀') .then( r => {
+                            msg.react('▶')
+                          
+                            const backwardsFilter = (reaction, user) => reaction.emoji.name === '⏪' && user.id === message.author.id;
+                            const forwardsFilter = (reaction, user) => reaction.emoji.name === '⏩' && user.id === message.author.id; 
+                          
+                            const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 }); 
+                            const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 }); 
+                          
+                           
+                            backwards.on('collect', r => { 
+                                if (page === 1) return; 
+                                page--; 
+                                embed.setDescription(pages[page-1]); 
+                                embed.setFooter(`Page ${page} of ${pages.length}`); 
+                                embed.clearReactions();
+                                msg.react('◀') 
+                                msg.react('▶')
+                                msg.edit(embed) 
+                            })
+                          
+                            forwards.on('collect', r => { 
+                                if (page === pages.length) return; 
+                                page++; 
+                                embed.setDescription(pages[page-1]); 
+                                embed.setFooter(`Page ${page} of ${pages.length}`);
+                                embed.clearReactions();
+                                msg.react('◀') 
+                                msg.react('▶')
+                                msg.edit(embed) 
+                            })
+                        })
+                    })
                 }
             })
         }  
