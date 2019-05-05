5eb29cbf1b02e0ebae55b7cb7413996cc0067659
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-05-05 16:31:32 +0800

    Update everyone.js

--- commands/everyone.js
+++ commands/everyone.js
@@ -3,10 +3,10 @@ const Discord = require('discord.js');
 module.exports.run = (bot, message, args, tools) => {
 
     let date = new Date()
-    let message = new Discord.RichEmbed()
+    let embed = new Discord.RichEmbed()
         .setDescription(`@everyone`)
         .addField(`${date} 更新快報`,args[1],true)
-    bot.channels.filter(c => c.id === arg[0]).forEach(c => c.send(message).then(m => message.push(m)));
+    bot.channels.filter(c => c.id === arg[0]).forEach(c => c.send(embed).then(m => message.push(m)));
 }
 
 module.exports.help = {
