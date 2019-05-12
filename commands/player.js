9889dc6a8ac40e7b7c58ef3ad8f33b3f97042d9c
patyhank <41978811+patyhank@users.noreply.github.com>
2019-05-12 20:11:03 +0800

    Update player.js

--- commands/player.js
+++ commands/player.js
@@ -55,8 +55,7 @@ module.exports.run = async (bot, message, args) => {
                   if (player.data[0].classes[0].name == "darkwizard") {
                     var role1 = `${mage} 黑暗巫師 / Dark Wizard`
                   }
-                  message.channel.send(role1)
-                  return
+                  message.channel.send(role1);
                 }
               }
             }
