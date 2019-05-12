b4428ca10d73921435f0d4590aa08b83d91d0804
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-05-12 16:54:03 +0800

    .

--- commands/guild.js
+++ commands/guild.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js")
 var request = require('request');
 
-const left = bot.emoji.get("577050509316456459")
-const right = bot.emoji.get("577050517335703553")
-
 module.exports.run = async (bot, message, args) => {
-  
+
+        const left = bot.emoji.get("577050509316456459")
+        const right = bot.emoji.get("577050517335703553")
+
         const guildName = args.join("");
         const urlMain = "https://api.wynncraft.com/public_api.php?action=guildStats&command=" + (guildName);
         if(guildName == ""){
