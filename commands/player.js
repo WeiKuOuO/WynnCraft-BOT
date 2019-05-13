790f6fc63ca08745bf75eb924c29b4341997c3ce
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-05-13 22:37:33 +0800

    Update player.js

--- commands/player.js
+++ commands/player.js
@@ -55,8 +55,34 @@ module.exports.run = async (bot, message, args) => {
                   if (player.data[0].classes[0].name == "darkwizard") {
                     var role1 = `${mage} 黑暗巫師 / Dark Wizard`
                   }
-                  console.log(role1);
-                  message.channel.send(role1);
+
+                  const xp0 = [
+                    player.data[0].classes[0].professions.combat,
+                    player.data[0].classes[0].professions.woodcutting,
+                    player.data[0].classes[0].professions.mining,
+                    player.data[0].classes[0].professions.fishing,
+                    player.data[0].classes[0].professions.farming,
+                    player.data[0].classes[0].professions.alchemism,
+                    player.data[0].classes[0].professions.armouring,
+                    player.data[0].classes[0].professions.cooking,
+                    player.data[0].classes[0].professions.jeweling,
+                    player.data[0].classes[0].professions.scribing,
+                    player.data[0].classes[0].professions.tailoring,
+                    player.data[0].classes[0].professions.weaponsmithing,
+                    player.data[0].classes[0].professions.woodworking,
+                    player.data[0].classes[0].professions.profession,
+                    player.data[0].classes[0].professions.overall
+                  ]
+
+                  const text = [
+                    `\`\`\`css\n戰鬥 / Combat | [${xp0[0].level} - ${xp0[0].xp}]\`\`\``
+                  ]
+
+                  let playerInfo = new Discord.RichEmbed()
+                  .setTitle(`${player.data[0].username} 的玩家資訊`)
+                  .addField(`${role1} 的等級資訊`, text, true)
+
+                  message.channel.send(playerInfo);
                 }
               }
             }
@@ -70,33 +96,13 @@ module.exports.run = async (bot, message, args) => {
 
 // 
 //                   if (player.data[0].classes[1].name == "assassin") {
-//                     const xp0 = [
-//                       player.data[0].classes[0].professions.combat,
-//                       player.data[0].classes[0].professions.woodcutting,
-//                       player.data[0].classes[0].professions.mining,
-//                       player.data[0].classes[0].professions.fishing,
-//                       player.data[0].classes[0].professions.farming,
-//                       player.data[0].classes[0].professions.alchemism,
-//                       player.data[0].classes[0].professions.armouring,
-//                       player.data[0].classes[0].professions.cooking,
-//                       player.data[0].classes[0].professions.jeweling,
-//                       player.data[0].classes[0].professions.scribing,
-//                       player.data[0].classes[0].professions.tailoring,
-//                       player.data[0].classes[0].professions.weaponsmithing,
-//                       player.data[0].classes[0].professions.woodworking,
-//                       player.data[0].classes[0].professions.profession,
-//                       player.data[0].classes[0].professions.overall
-//                     ]
+
 //                     const xp = [
 //                       xp0
 //                     ]
-//                     const text = [
-//                       `\`\`\`css\n戰鬥 / Combat | [${xp[0][0].level} - ${xp[0][0].xp}]\`\`\``
-//                     ]
+
 //                     var role1 = ":dagger: 刺客 / Assassin"
-//                     let playerInfo = new Discord.RichEmbed()
-//                       .setTitle(`${player.data[0].username} 的玩家資訊`)
-//                       .addField(`${role1} 的等級資訊`, text, true)
+
 //                     essage.channel.send(playerInfo)
 //                   }
 
