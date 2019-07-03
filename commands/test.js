f88c2df151426e4595ea83779fcf94a078bf8bac
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-07-03 14:56:05 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -109,36 +109,38 @@ module.exports.run = async (bot, message, args) => {
                 ]   
             }
             const reactmsg = await message.channel.send(pages[0]).catch(e => {})
-                await reactmsg.react(right)//.then(() => reactmsg.react(right))
-                // const filter1 = (reaction, user) => reaction.emoji.name == left && message.author.id == user.id
-                // const collector1 = reactmsg.createReactionCollector(filter1, {time: 600000});
+                await reactmsg.react(left).then(() => reactmsg.react(right))
+                const filter1 = (reaction, user) => reaction.emoji.name == left && message.author.id == user.id
+                const collector1 = reactmsg.createReactionCollector(filter1, {time: 600000});
     
-                const filter = (reaction, user) => reaction.emoji.name == right && message.author.id == user.id
-                const collector = reactmsg.createReactionCollector(filter, {time: 600000});
+                const filter2 = (reaction, user) => reaction.emoji.name == right && message.author.id == user.id
+                const collector2 = reactmsg.createReactionCollector(filter2, {time: 600000});
     
-	              // collector1.on('collect', async reaction => {
-                //   const user = reaction.users.last()
-                //   reaction.remove(user)
-                //   if (page == 1) return; 
-                //   page--; 
-                //   pages[page-1].setFooter(`頁數 | ${page} / ${pages.length}`); 
-                //   await reactmsg.edit(pages[page-1])
-                // })
-
-                collector.on('collect', async reaction => {
+	              collector1.on('collect', async reaction => {
                   const user = reaction.users.last()
                   reaction.remove(user)
-                  if (page == pages.length) return; 
-                  page++; 
-                  pages[page-1].setFooter(`頁數 | ${page} / ${pages.length}`); 
-                  await reactmsg.edit(pages[page-1])
+                  message.channel.send("1")
+                  // if (page == 1) return; 
+                  // page--; 
+                  // pages[page-1].setFooter(`頁數 | ${page} / ${pages.length}`); 
+                  // await reactmsg.edit(pages[page-1])
                 })
 
-                // collector1.on('end', collected => {
-                //   reactmsg.clearReactions()
-                // })
+                collector2.on('collect', async reaction => {
+                  const user = reaction.users.last()
+                  reaction.remove(user)
+                  message.channel.send("2")
+                  // if (page == pages.length) return; 
+                  // page++; 
+                  // pages[page-1].setFooter(`頁數 | ${page} / ${pages.length}`); 
+                  // await reactmsg.edit(pages[page-1])
+                })
 
-                collector.on('end', collected => {
+                collector1.on('end', collected => {
+                  reactmsg.clearReactions()
+                })
+
+                collector2.on('end', collected => {
                   reactmsg.clearReactions()
                 })
         })
