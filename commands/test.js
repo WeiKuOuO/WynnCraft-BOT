3a85d51a962d99ce8e71138f6662679a793a53b3
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-07-03 13:45:01 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,55 +1,29 @@
-const Discord = require('discord.js'); 
- 
-module.exports.run = (bot, message, args, tools) => {
- 
-  let pages = [
-   'test1 fuck u', 
-   'test2 fuck u 2', 
-   'test3 did u think i will say fuck u 3?', 
-   'test4 Maybe', 
-   '**test5 I want to say fuck u 10**'
-  ]; 
-  let page = 1; 
- 
-  const embed = new Discord.RichEmbed() 
-    .setColor(0xffffff)
-    .setFooter(`Page ${page} of ${pages.length}`) 
-    .setDescription(pages[page-1])
- 
-  message.channel.send(embed).then(msg => { 
-   
-    msg.react('⏪').then( r => { 
-      msg.react('⏩') 
-      
-      const backwardsFilter = (reaction, user) => reaction.emoji.name === '⏪' && user.id === message.author.id;
-      const forwardsFilter = (reaction, user) => reaction.emoji.name === '⏩' && user.id === message.author.id; 
-     
-      const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 }); 
-      const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 }); 
-     
-      
-      backwards.on('collect', r => { 
-        if (page === 1) return; 
-        page--; 
-        embed.setDescription(pages[page-1]); 
-        embed.setFooter(`Page ${page} of ${pages.length}`); 
-        msg.edit(embed) 
-      })
-     
-      forwards.on('collect', r => { 
-        if (page === pages.length) return; 
-        page++; 
-        embed.setDescription(pages[page-1]); 
-        embed.setFooter(`Page ${page} of ${pages.length}`); 
-        msg.edit(embed) 
-      })
-   
+const Discord = require("discord.js")
+const fs = require('fs');
+
+module.exports.run = async (bot, msg, args) => {
+    const embed = new Discord.RichEmbed()
+            .setTitle("test")
+            .setFooter(msg.author.username, msg.author.avatarURL)
+            .setColor("RANDOM")
+    const reactmsg = await msg.channel.send(embed).catch(e => {})
+    await reactmsg.react('🆕')
+    const filter = (reaction, user) => reaction.emoji.name === '🆕' && msg.author.id == user.id
+	const collector = reactmsg.createReactionCollector(filter, {time: 600000});
+	collector.on('collect', async reaction => {
+        const user = reaction.users.last()
+        reaction.remove(user)
+        const embed = new Discord.RichEmbed()
+            .setTitle("test")
+            .setFooter(user.username, user.avatarURL)
+            .setColor("RANDOM")
+        await reactmsg.edit(embed)
     })
- 
-  })
- 
+    collector.on('end', collected => {
+        reactmsg.clearReactions()
+    })
 }
 
-module.exports.help = {
-    name: "test"
+module.exports.config = {
+    command: "no",
 }
