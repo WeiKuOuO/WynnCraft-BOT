4b5a2eb71de3740a8bb2c8b83dc9b2b5c10a7274
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-08-21 16:05:10 +0800

    Update guildtest.js

--- commands/guildtest.js
+++ commands/guildtest.js
@@ -6,7 +6,6 @@ module.exports.run = async (bot, message, args) => {
         const left = bot.emojis.get("577050509316456459")
         const right = bot.emojis.get("577050517335703553")
 
-
         const guildName = args.join("");
         const urlMain = "https://api.wynncraft.com/public_api.php?action=guildStats&command=" + (guildName);
         if(guildName == ""){
@@ -22,30 +21,13 @@ module.exports.run = async (bot, message, args) => {
                 if(guild.error){
                     message.channel.send("你輸入了錯誤的公會名稱,可能是大小寫錯誤了")
                 }else{
-                    var guildimages = (function() {
-                        loadGuildModal()
-                    })
-                    function loadGuildModal(t) {
-                        $("#guildModal").modal({
-                            show: !0
-                        }), guild + t, function(t) {
-                            if (t.hasOwnProperty("banner")) {
-                                $(".guildInformationContainer").addClass("col-xs-6");
-                                for (var e = {
-                                        image: ["url('https://wynncraft.com/img/banners/bg_base.png')"],
-                                        repeat: ["no-repeat"],
-                                        position: ["0 0"]
-                                    }, a = t.banner.layers.length - 1; a >= 0; a--) e.image.push("url('https://wynncraft.com/img/banners/" + t.banner.layers[a].colour.toLowerCase() + ".png')"), e.repeat.push("no-repeat"), e.position.push(banner_positions[t.banner.layers[a].pattern]);
-                                e.image.push("url('https://wynncraft.com/img/banners/" + t.banner.base.toLowerCase() + ".png')"), e.repeat.push("no-repeat"), e.position.push(banner_positions.BASE), $("#banner").css("background-image", e.image.join(",")), $("#banner").css("background-repeat", e.repeat.join(",")), $("#banner").css("background-position", e.position.join(","))
-                            } else $(".bannerRenderContainer").hide(), $(".guildInformationContainer").removeClass("col-xs-6");
-                        }
-                    }
                     let tmp1;
                     let tmp2;
                     let tmp3;
                     let tmp4;
                     let tmp5;
                     guild.members.forEach(function(member) {
+                        
                         if (member.rank === "OWNER") {
                             if (tmp1 == null) {
                                 tmp1 = member.name;
@@ -53,6 +35,8 @@ module.exports.run = async (bot, message, args) => {
                                 tmp1 = tmp1 + "\n" + member.name;
                             }
                         }
+
+                        
                         if (member.rank === "CHIEF") {
                             if (tmp2 == null) {
                                 tmp2 = member.name;
@@ -60,6 +44,8 @@ module.exports.run = async (bot, message, args) => {
                                 tmp2 = tmp2 + "\n" + member.name;
                             }
                         }
+
+                        
                         if (member.rank === "CAPTAIN") {
                             if (tmp3 == null) {
                                 tmp3 = member.name;
@@ -67,6 +53,8 @@ module.exports.run = async (bot, message, args) => {
                                 tmp3 = tmp3 + "\n" + member.name;
                             }
                         }
+
+                        
                         if (member.rank === "RECRUITER") {
                             if (tmp4 == null) {
                                 tmp4 = member.name;
@@ -74,6 +62,8 @@ module.exports.run = async (bot, message, args) => {
                                 tmp4 = tmp4 + "\n" + member.name;
                             }
                         }
+
+                    
                         if (member.rank === "RECRUIT") {
                             if (tmp5 == null) {
                                 tmp5 = member.name;
@@ -89,7 +79,7 @@ module.exports.run = async (bot, message, args) => {
                     let guildInfo = new Discord.RichEmbed()
                         .setColor(0x34AB00)
                         .setTitle(`${guild.name} 的資訊`)
-                        .setImage(guildimages)
+                        .setThumbnail(`https://mysterious-ridge-74146.herokuapp.com/images/${guild.name}.png`)
                         .addField(":pager:  工會名稱",`\`\`\`css\n${guild.name}\`\`\``,true)
                         .addField(":mega: 公會前綴",`\`\`\`md\n#${guild.prefix}\`\`\``,true)
                         .addField(":evergreen_tree: 公會等級",`\`\`\`diff\n+    Level${guild.level}   +\`\`\``,true)
@@ -120,78 +110,34 @@ module.exports.run = async (bot, message, args) => {
                     ]
                     
 
-                    const reactionControls = {
-                        NEXT_PAGE: '▶',
-                        PREV_PAGE: '◀'
-                    }
-                    
-                    const collector = new Discord.ReactionCollector(message, (reaction, user) => Object.values(reactionControls).includes(reaction.emoji.name), {
-                        time: 60000, // stop automatically after one minute 
-                    });
-                        
-
-                    message.channel.send(pages[page-1]).then(msg => { 
-
-                        msg.react("▶").then( r => { 
-                            msg.react("◀") 
-                          
-
-                        collector.on('collect', (reaction, user) => {
-                            switch (reaction.emoji.name) {
-                                case emojis.NEXT_PAGE: {
-                                    if (page === 1) return; 
-                                    page--; 
-                                    pages[page-1].setFooter(`頁數 | ${page} / ${pages.length}`); 
-                                    msg.edit(pages[page-1])
-                                    message.clearReactions();
-                                    break;
-                                }
-                                case emojis.PREV_PAGE: {
-                                    if (page === pages.length) return; 
-                                    page++; 
-                                    pages[page-1].setFooter(`頁數 | ${page} / ${pages.length}`); 
-                                    msg.edit(pages[page-1]) 
-                                    message.clearReactions();
-                                    break;
-                                }
-                            }     
-                        });
-                        })
-                    });
                 
+                    message.channel.send(pages[page-1]).then(msg => {
+                    
+                        msg.react(left).then( r => { 
+                            msg.react(right) 
+                          
+                            const backwardsFilter = (reaction, user) => reaction.emoji.name === left && user.id === message.author.id;
+                            const forwardsFilter = (reaction, user) => reaction.emoji.name === right && user.id === message.author.id; 
+                          
+                            const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 }); 
+                            const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 }); 
 
-                    
-                    
-                    // message.channel.send(pages[page-1]).then(msg => { 
-                    
-                    //     msg.react(left).then( r => { 
-                    //         msg.react(right) 
+                            backwards.on('collect', r => { 
+                                if (page === 1) return; 
+                                page--; 
+                                pages[page-1].setFooter(`頁數 | ${page} / ${pages.length}`); 
+                                msg.edit(pages[page-1]) 
+                            })
                           
-                    //         const backwardsFilter = (reaction, user) => reaction.emoji.name === '⏪' && user.id === message.author.id;
-                    //         const forwardsFilter = (reaction, user) => reaction.emoji.name === '⏩' && user.id === message.author.id; 
-                          
-                    //         const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 }); 
-                    //         const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 }); 
-                          
-                           
-                    //         backwards.on('collect', r => { 
-                                // if (page === 1) return; 
-                                // page--; 
-                                // pages[page-1].setFooter(`頁數 | ${page} / ${pages.length}`); 
-                                // msg.edit(pages[page-1]) 
-                    //         })
-                          
-                    //          forwards.on('collect', r => { 
-                    //             if (page === pages.length) return; 
-                    //             page++; 
-                    //             pages[page-1].setFooter(`頁數 | ${page} / ${pages.length}`); 
-                    //             msg.edit(pages[page-1]) 
-                    //         })
-                        
-                    //     })
-                      
-                    // })
-
+                            forwards.on('collect', r => { 
+                                if (page === pages.length) return; 
+                                page++; 
+                                pages[page-1].setFooter(`頁數 | ${page} / ${pages.length}`); 
+                                msg.edit(pages[page-1]) 
+                            })
+                            
+                        })
+                    })
                 }
             })
         }  
