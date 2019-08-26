44f94ad4f52ac17e3fc4c18f05e6aab79678fcae
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-08-26 15:31:15 +0800

    Revert "Update guild.js"
    This reverts commit 7b33648de6be26eaf563476bd49ee1b85db1888e.

--- commands/guild.js
+++ commands/guild.js
@@ -119,17 +119,14 @@ module.exports.run = async (bot, message, args) => {
                             const backwardsFilter = (reaction, user) => reaction.emoji.id === "577050509316456459" && user.id === message.author.id;
                             const forwardsFilter = (reaction, user) => reaction.emoji.id === "577050517335703553" && user.id === message.author.id; 
                           
-                            const backwards = msg.createReactionCollector(backwardsFilter, { time: 180000 }); 
-                            const forwards = msg.createReactionCollector(forwardsFilter, { time: 180000 }); 
+                            const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 }); 
+                            const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 }); 
 
                             backwards.on('collect', r => { 
                                 if (page === 1) return; 
                                 page--; 
                                 pages[page-1].setFooter(`頁數 | ${page} / ${pages.length}`); 
                                 msg.edit(pages[page-1]) 
-                                msg.clearReactions();
-                                msg.react(left).then( r => { 
-                                    msg.react(right) })
                             })
                           
                             forwards.on('collect', r => { 
@@ -137,9 +134,6 @@ module.exports.run = async (bot, message, args) => {
                                 page++; 
                                 pages[page-1].setFooter(`頁數 | ${page} / ${pages.length}`); 
                                 msg.edit(pages[page-1]) 
-                                msg.clearReactions();
-                                msg.react(left).then( r => { 
-                                    msg.react(right)})
                             })
                             
                         })
