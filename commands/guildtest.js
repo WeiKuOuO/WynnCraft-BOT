87ca9eb44f9d88c4e5e81c8723d961ef73edb025
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2019-08-26 15:36:13 +0800

    .

--- commands/guildtest.js
+++ commands/guildtest.js
@@ -123,7 +123,6 @@ module.exports.run = async (bot, message, args) => {
                             const forwards = msg.createReactionCollector(forwardsFilter, { time: 180000 }); 
 
                             backwards.on('collect', r => { 
-<<<<<<< HEAD
                                 if (page === 1){
                                     msg.clearReactions();
                                     msg.react(left)
@@ -147,22 +146,6 @@ module.exports.run = async (bot, message, args) => {
                                 msg.edit(pages[page-1]) 
                                 msg.clearReactions();
                                 msg.react(left).then( r => {msg.react(right)}) 
-=======
-                                if (page === 1) return
-                                page--; 
-                                pages[page-1].setFooter(`頁數 | ${page} / ${pages.length}`); 
-                                msg.edit(pages[page-1]) 
-                                msg.reaction.remove(use.id)
-                            })
-                          
-                            forwards.on('collect', r => { 
-                                if (page === pages.length) return
-                                page++; 
-                                pages[page-1].setFooter(`頁數 | ${page} / ${pages.length}`); 
-                                msg.edit(pages[page-1]) 
-                                msg.reaction.remove(use.id)
-                                
->>>>>>> parent of bcc55d1... .
                             })
                             
                         })
